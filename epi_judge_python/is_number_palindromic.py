66e5b07ec76b8c036f46288491aeba9016906e59
Kyle Coralejo <kylecjo@gmail.com>
2019-07-01 19:20:46 -0700

    July 1 Practice

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,21 @@
 from test_framework import generic_test
-
+from math import floor, log10
 
 def is_palindrome_number(x):
     # TODO - you fill in here.
+    if x <= 0:
+        return x == 0
+    
+    num_digits = floor(log10(x)) + 1
+    MSB_MASK = 10 ** (num_digits -1)
+
+    for i in range(num_digits // 2):
+        if x // MSB_MASK != x % 10:
+            return False
+        x %= MSB_MASK
+        x //= 10
+
+        MSB_MASK //= 100  
     return True
 
 
