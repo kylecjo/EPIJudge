0f69498079ed4e8ee66e95db25b16b33592ff220
Kyle Coralejo <kylecjo@gmail.com>
2019-07-04 20:32:57 -0700

    July 4 Practice

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,7 +9,32 @@ RED, WHITE, BLUE = range(3)
 
 def dutch_flag_partition(pivot_index, A):
     # TODO - you fill in here.
-    return
+    pivot = A[pivot_index]
+    # for i in range(len(A)):
+    #     for j in range(i + 1, len(A)):
+    #         if A[j] < pivot:
+    #             A[i], A[j] = A[j], A[i]               
+    #             break
+
+    # for i in reversed(range(len(A))):
+    #     for j in range(i):
+    #         if A[j] > pivot:
+    #             A[i], A[j] = A[j], A[i]               
+    #             break
+
+    smaller = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller += 1
+
+    larger = len(A) -1
+    for j in reversed(range(len(A))):
+        if A[j] > pivot:
+            A[j], A[larger] = A[larger], A[j]
+            larger -= 1
+    print(A)
+    return A
 
 
 @enable_executor_hook
